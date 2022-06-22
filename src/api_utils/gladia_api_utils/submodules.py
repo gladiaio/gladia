0a3287a056a203f20097d6ccaa55d52d6eb937ad
Thytu <valentin.de-matos@epitech.eu>
2022-06-22 17:37:02 +0000

    refactor: submodules now print errors when subprocess fail

--- src/api_utils/gladia_api_utils/submodules.py
+++ src/api_utils/gladia_api_utils/submodules.py
@@ -131,6 +131,8 @@ def exec_in_custom_env(path_to_env_file: str, cmd: str):
         process = subprocess.Popen(full_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, executable='/bin/bash')
         output, error = process.communicate()
 
+        print("[error]:", error)
+
     except subprocess.CalledProcessError as error:
         raise RuntimeError(f"Couldn't activate custom env {env_name}: {error}")
 

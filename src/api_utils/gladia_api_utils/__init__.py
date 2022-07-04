208aab236f5a665153d8271f057a7934b2e2c89a
Thytu <valentin.de-matos@epitech.eu>
2022-07-04 08:35:02 +0000

    feat: TorchvisionModel wrapper

--- src/api_utils/gladia_api_utils/__init__.py
+++ src/api_utils/gladia_api_utils/__init__.py
@@ -1,6 +1,6 @@
 from .get_activated_task_path import get_activated_task_path
-
+from .TorchvisionModel import TorchvisionModel
 
 __all__ = [
-    "get_activated_task_path",
+    "get_activated_task_path", "TorchvisionModel"
 ]

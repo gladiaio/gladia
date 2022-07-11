7fc6dca3a9311e72f75efa1df8187d7d8ee96626
Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>
2022-07-11 16:09:42 +0000

    fix(CoquiEngineHelper): fix init in CoquiEngineHelper

--- src/api_utils/gladia_api_utils/CoquiEngineHelper.py
+++ src/api_utils/gladia_api_utils/CoquiEngineHelper.py
@@ -1,37 +1,52 @@
 import wave
 from io import BytesIO
+from pathlib import Path
 
 import ffmpeg
 import numpy as np
-from stt import Model
 from gladia_api_utils.model_management import download_models
-from pathlib import Path
 from icecream import ic
+from stt import Model
+
 
 class SpeechToTextEngine:
-    def __init__(self, model_uri: str="english/coqui/v1.0.0-huge-vocab", model="model.tflite", scorer="scorer.scorer"):
+    def __init__(
+        self,
+        model_uri: str = "english/coqui/v1.0.0-huge-vocab",
+        model: str = "model.tflite",
+        scorer: str = "huge-vocabulary.scorer",
+    ):
 
         language, author, version = model_uri.split("/")
-        
+
         model_url = f"https://coqui.gateway.scarf.sh/{model_uri}"
 
-        models_path = download_models({
-            f"{model_uri}/{model}": {
-                "url": f"{model_url}/{model}",
-                "output_path": "model",
-            },
-            f"{model_uri}/{scorer}": {
-                "url": f"{model_url}/{scorer}",
-                "output_path": "scorer",
+        models_path = download_models(
+            {
+                f"{model_uri}/{model}": {
+                    "url": f"{model_url}/{model}",
+                    "output_path": "model",
+                },
+                f"{model_uri}/{scorer}": {
+                    "url": f"{model_url}/{scorer}",
+                    "output_path": "scorer",
+                },
             }
-        })
-        
-        model_path=Path(models_path[f"{model_uri}/{model}"]["output_path"]).absolute().as_posix()
-        scorer_path=Path(models_path[f"{model_uri}/{scorer}"]["output_path"]).absolute().as_posix()
+        )
+
+        model_path = (
+            Path(models_path[f"{model_uri}/{model}"]["output_path"])
+            .absolute()
+            .as_posix()
+        )
+        scorer_path = (
+            Path(models_path[f"{model_uri}/{scorer}"]["output_path"])
+            .absolute()
+            .as_posix()
+        )
 
         self.model = Model(model_path)
         self.model.enableExternalScorer(scorer_path)
-
 
     def normalize_audio(self, audio):
         out, err = (
@@ -50,7 +65,6 @@ class SpeechToTextEngine:
         if err:
             raise Exception(err)
         return out
-
 
     def run(self, audio):
         audio = self.normalize_audio(audio)

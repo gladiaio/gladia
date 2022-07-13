cd3ea155aa8f0769b7f26f2e10fe3e65b66d8e3b
Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>
2022-07-13 08:27:34 -0700

    Update CoquiEngineHelper.py

--- src/api_utils/gladia_api_utils/CoquiEngineHelper.py
+++ src/api_utils/gladia_api_utils/CoquiEngineHelper.py
@@ -1,30 +1,13 @@
-<<<<<<< HEAD
-from tkinter.tix import Tree
-
-from genericpath import exists
-
-=======
->>>>>>> c28c1fa0fa967eff1da360944095fd0950e49059
 import wave
 from io import BytesIO
 from pathlib import Path
 
 import ffmpeg
 import numpy as np
-
-<<<<<<< HEAD
-import os
-
 from gladia_api_utils.model_management import download_model
 from icecream import ic
 from stt import Model
-
-=======
-from gladia_api_utils.model_management import download_models
-from icecream import ic
-from stt import Model
-
->>>>>>> c28c1fa0fa967eff1da360944095fd0950e49059
+import os
 
 class SpeechToTextEngine:
     def __init__(
@@ -36,7 +19,6 @@ class SpeechToTextEngine:
 
         language, author, version = model_uri.split("/")
 
-<<<<<<< HEAD
         model_path_prefix = f"{os.getenv('MODEL_CACHE_ROOT')}/audio/text/{model_uri}"
 
         model_url = f"https://coqui.gateway.scarf.sh/{model_uri}"
@@ -61,30 +43,6 @@ class SpeechToTextEngine:
 
         scorer_path = (
             Path(scorer_path)
-=======
-        model_url = f"https://coqui.gateway.scarf.sh/{model_uri}"
-
-        models_path = download_models(
-            {
-                f"{model_uri}/{model}": {
-                    "url": f"{model_url}/{model}",
-                    "output_path": "model",
-                },
-                f"{model_uri}/{scorer}": {
-                    "url": f"{model_url}/{scorer}",
-                    "output_path": "scorer",
-                },
-            }
-        )
-
-        model_path = (
-            Path(models_path[f"{model_uri}/{model}"]["output_path"])
-            .absolute()
-            .as_posix()
-        )
-        scorer_path = (
-            Path(models_path[f"{model_uri}/{scorer}"]["output_path"])
->>>>>>> c28c1fa0fa967eff1da360944095fd0950e49059
             .absolute()
             .as_posix()
         )

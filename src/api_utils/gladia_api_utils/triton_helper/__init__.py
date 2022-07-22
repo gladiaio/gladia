d9e2cb9de41ff2c45c5566c322324dce13c78404
Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>
2022-07-22 23:41:01 +0200

    Fix(init): import data_preprocessing

--- src/api_utils/gladia_api_utils/triton_helper/__init__.py
+++ src/api_utils/gladia_api_utils/triton_helper/__init__.py
@@ -1,4 +1,4 @@
-from .download_active_models import download_active_triton_models, download_triton_model
+from .download_active_models import download_active_triton_models, download_triton_model, data_preprocessing
 from .TritonClient import TritonClient
 
-__all__ = ["download_triton_model", "download_active_triton_models", "TritonClient"]
+__all__ = ["download_triton_model", "download_active_triton_models", "TritonClient", "data_preprocessing"]

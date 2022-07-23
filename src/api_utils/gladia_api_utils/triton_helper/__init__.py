22759dfb25936f6c10e0ecdf558af82891706ec2
Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>
2022-07-23 07:14:41 +0200

    fix(init): wrong path for data_preprocessing

--- src/api_utils/gladia_api_utils/triton_helper/__init__.py
+++ src/api_utils/gladia_api_utils/triton_helper/__init__.py
@@ -1,9 +1,9 @@
 from .download_active_models import (
-    data_preprocessing,
     download_active_triton_models,
     download_triton_model,
 )
 from .TritonClient import TritonClient
+from .data_preprocessing import data_preprocessing
 
 __all__ = [
     "download_triton_model",

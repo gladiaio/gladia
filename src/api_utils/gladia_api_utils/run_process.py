7aea963b1ee50b88e9cc37a2563979ddc1659953
Olivier Boulant <olivier.boulant@hey.com>
2022-07-25 15:26:25 +0000

    feat(run_process): add usage instructions and support bytes outputs

--- src/api_utils/gladia_api_utils/run_process.py
+++ src/api_utils/gladia_api_utils/run_process.py
@@ -6,7 +6,20 @@ import urllib.parse
 
 from PIL import Image
 
+HELP_STRING = """
+python <PATH_TO_FILE>/run_process.py <module_path> <model> <output_tmp_result> <kwargs for predict>
+    - module_path : the route to the targeted model (for instance `apis/image/image/face-blurings/ageitgey/`)
+    - model : the targeted model name (for instance `ageitgey`)
+    - output_tmp_result : the path to where the results will be written (for example `/tmp/tmpo8q0coqe`)
+    - kwargs : a dictionary encoded with `urllib.parse.quote()`. Keywords argument are needed for the `predict` function (for instance `{'image': '/tmp/tmp342by415'}` is encoded into `%257B%2522image%2522%253A%2520%2522/tmp/tmp342by415%2522%257D`)
+"""
+
 if __name__ == "__main__":
+
+    if len(sys.argv) < 5:
+        print("Not enough arguments. Please read usage below.")
+        print(HELP_STRING)
+        sys.exit(1)
 
     module_path = sys.argv[1]
     model = sys.argv[2]
@@ -31,6 +44,9 @@ if __name__ == "__main__":
 
     if isinstance(output, Image.Image):
         output.save(f"{output_tmp_result}", format="PNG")
+    elif isinstance(output, bytes):
+        with open(f"{output_tmp_result}", "wb") as f:
+            f.write(output)
     else:
         with open(f"{output_tmp_result}", "w") as f:
             f.write(str(output))

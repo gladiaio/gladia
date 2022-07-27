9831a16bcacbb4524c2088c74fd2dfdb2863ff4d
Thytu <valentin.de-matos@epitech.eu>
2022-07-27 14:30:51 +0000

    refactor(TritonHelper): moved check_if_model_needs_to_be_preloaded to another file

--- src/api_utils/gladia_api_utils/triton_helper/TritonClient.py
+++ src/api_utils/gladia_api_utils/triton_helper/TritonClient.py
@@ -1,10 +1,10 @@
-import json
 import os
 import pathlib
 import sys
 from time import sleep
 from typing import Any
 from warnings import warn
+from .helper import check_if_model_needs_to_be_preloaded
 
 import requests
 import tritonclient.http as tritonclient
@@ -45,7 +45,7 @@ class TritonClient:
         self.__model_name = model_name
         self.__model_sub_parts = kwargs.get("sub_parts", [])
 
-        self.__preload_model: bool = self.check_if_model_needs_to_be_preloaded()
+        self.__preload_model: bool = check_if_model_needs_to_be_preloaded(self.__model_name)
 
         if self.__preload_model and not self.load_model():
             warn(
@@ -141,35 +141,6 @@ class TritonClient:
             )
         )
 
-    def check_if_model_needs_to_be_preloaded(self) -> bool:
-        """Check if the model needs to be preloaded according to the GladIA's config file
-
-        Returns:
-            bool: whether the model needs to be preloaded
-        """
-        path_to_config_file = os.getenv("API_CONFIG_FILE", "config.json")
-
-        if not os.path.isfile(path_to_config_file):
-            warn(
-                f"[TritonClient] Couldn't load config file {path_to_config_file}, setting __preload_model to False."
-            )
-
-            return False
-
-        with open(path_to_config_file, "r") as f:
-            config_file = json.load(f)
-
-        if (
-            "triton" not in config_file.keys()
-            or "models_to_preload" not in config_file["triton"].keys()
-        ):
-            warn(
-                f"[TritonClient] Couldn't find 'models_to_preload' param key in the config file {path_to_config_file}, setting __preload_model to False."
-            )
-
-            return False
-
-        return self.__model_name in config_file["triton"]["models_to_preload"]
 
     def register_new_output(self, **kwargs) -> None:
         """Add a new output to the triton inferer. Each ouput has to be registered before usage.\n
